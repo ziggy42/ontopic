b9f0fae3256dc7ee41f082b9a3a1762ae44c5035
Andrea Pivetta <vanpivix@gmail.com>
2020-02-03 19:05:32 +0000

    Fetch AWS region from environment

--- cli.js
+++ cli.js
@@ -15,8 +15,9 @@ const parser = new ArgumentParser({
 parser.addArgument(
     ['-r', '--region'],
     {
-        help: `AWS region to use. Defaults to ${DEFAULT_AWS_REGION}.`,
-        defaultValue: DEFAULT_AWS_REGION,
+        help: 'AWS region to use for the queue. If absent, it will be set to the environment variable '
+            + `AWS_DEFAULT_REGION, AWS_REGION or defaulted to ${DEFAULT_AWS_REGION}.`,
+        defaultValue: process.env.AWS_DEFAULT_REGION || process.env.AWS_REGION || DEFAULT_AWS_REGION
     }
 );
 

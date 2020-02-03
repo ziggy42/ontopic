541d2771a9d92533e048d19f87dfe1da57cb2d30
Andrea Pivetta <vanpivix@gmail.com>
2020-02-03 22:16:49 +0000

    More logs, more error handling

--- src/ontopic.js
+++ src/ontopic.js
@@ -4,6 +4,26 @@ const { logMessage } = require('./logger');
 let QueueUrl;
 let SubscriptionArn;
 
+const removeResources = async () => {
+    try {
+        if (SubscriptionArn) {
+            console.log(`Deleting ${SubscriptionArn}`);
+            await deleteSubscription(SubscriptionArn);
+        }
+
+        if (QueueUrl) {
+            console.log(`Deleting ${QueueUrl}`);
+            await deleteQueue(QueueUrl);
+        }
+    } catch (err) {
+        console.log(`An error occurred while cleaning up resources: ${err.message}`);
+        console.log('You may need to manually remove the following resources:');
+        [SubscriptionArn, QueueUrl]
+            .filter((resource) => resource)
+            .forEach((resource) => console.log(`- ${resource}`));
+    }
+};
+
 const listen = async ({ topicArn, region }) => {
     const accountId = await getAccountId();
     const queue = await createQueue(topicArn, region, accountId);
@@ -15,24 +35,21 @@ const listen = async ({ topicArn, region }) => {
     SubscriptionArn = subscription.arn;
 
     console.log('Waiting for messages..');
-    // eslint-disable-next-line no-constant-condition
-    while (true) {
-        const messages = await getMessages(queue.url);
-        messages.forEach((message) => logMessage(message));
+    try {
+        // eslint-disable-next-line no-constant-condition
+        while (true) {
+            const messages = await getMessages(queue.url);
+            messages.forEach((message) => logMessage(message));
+        }
+    } catch (err) {
+        console.log(`An error occurred while polling new messages: ${err.message}`);
+        console.log('Attempting to clean up resources...');
+        await removeResources();
     }
 };
 
 ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGABRT', 'SIGTERM'].forEach((signal) => process.on(signal, async () => {
-    if (SubscriptionArn) {
-        console.log('Deleting ' + SubscriptionArn);
-        await deleteSubscription(SubscriptionArn);
-    }
-
-    if (QueueUrl) {
-        console.log('Deleting ' + QueueUrl);
-        await deleteQueue(QueueUrl);
-    }
-
+    await removeResources();
     process.exit(0);
 }));
 

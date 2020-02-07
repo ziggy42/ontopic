c564af60c361b39fcacceb0ce42e62aa0cd38a7a
Andrea Pivetta <vanpivix@gmail.com>
2020-02-07 21:19:40 +0100

    Take program name, description from package.json (#4)

--- cli.js
+++ cli.js
@@ -2,14 +2,15 @@
 
 const { ArgumentParser } = require('argparse');
 const { listen } = require('./src/ontopic');
-const { version } = require('./package.json');
+const { version, description, name } = require('./package.json');
 
 const DEFAULT_AWS_REGION = 'eu-central-1';
 
 const parser = new ArgumentParser({
-    version,
     addHelp: true,
-    description: 'Listen to SNS notifications'
+    prog: name,
+    description,
+    version
 });
 
 parser.addArgument(
